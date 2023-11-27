752b6b333f7c4d4c7cee24794fc18d58a02d09c2
lilshudd <max.fedorak10@gmail.com>
2023-11-27 12:57:22 +0200

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </div>
+      `;
+    })
+    .join('');
+}
+
+galleryContainer.innerHTML = createGalleryMarkup();
+
+const galleryImages = galleryContainer.querySelectorAll('.gallery__image');
+
+galleryImages.forEach((image, index) => {
+  image.addEventListener('click', event => {
+    event.preventDefault();
+
+    const lightbox = basicLightbox.create(`
+      <img src="${galleryItems[index].original}" alt="${galleryItems[index].description}" />
+    `);
+    lightbox.show();
+  });
+});
 
 console.log(galleryItems);
